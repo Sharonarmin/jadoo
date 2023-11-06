ec50411336ed6d0c0f21866d219c77e6570947e7
unknown <sharonarmin84@gmail.com>
2023-11-06 20:28:03 +0530

    ss

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+// svg fill
+document.getElementById("my-svg").addEventListener("click", function() {
+    var path = this.querySelector("path");
+    path.setAttribute("fill", "red");
+});
+ 
+// client slider
+$('.client-slider').owlCarousel({
+  loop:true,
+  margin:10,
+ autoplay:true,
+  nav:false,
+  responsive:{
+      0:{
+          items:2
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+
+// aos
+AOS.init();
+
+// swiper
+
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
